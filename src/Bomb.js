38c52d977e150c21f4f455b640119e090bc89670
KaladaJumbo <wkalada1@gmail.com>
2020-10-26 12:16:42 -0400

    final commit

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,31 @@
-// your Bomb code here!
+// your Bomb code here
+import React, {Component} from 'react';
+
+export default class Bomb extends Component {
+    state = {
+        secondsLeft: this.props.initialCount
+    }
+
+    //call func here ... 
+
+    render () {
+        let bob = "";
+
+        if(this.state.secondsLeft == 0 ){
+            bob = "Boom!"
+        }
+        else{
+            bob = this.state.secondsLeft + " seconds left before I go boom!"
+    
+        }
+
+        return (
+            
+            <div>
+                {bob}
+            </div>
+        )
+    }
+
+}
+
